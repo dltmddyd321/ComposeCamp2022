47e4711727bb5576ba1de737f810699396764f4c
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2022-11-08 22:35:03 -0800

    UI 기본 / 리컴포저블/ 열과 행/ 호이스팅
    - 프리뷰와 기본 UI 설정
    - 컴포저블의 재사용
    - Row 및 Column 구성
    - 상태 호이스팅 기초

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -3,13 +3,13 @@ package com.codelab.basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.codelab.basics.ui.theme.BasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,13 +17,31 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BasicsCodelabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                TestApp()
+            }
+        }
+    }
+}
+
+//상태 호이스팅 : 컴포저블 함수에서 여러 함수가 읽거나 수정하는 상태는 공통 상위 항목에 위치해야한다.
+@Composable
+fun OnBoardingScreen(onContinueCLicked: () -> Unit) {
+    //by 키워드를 통해 .value를 기입할 필요가 없다.
+    var shouldShowOnBoarding by remember {
+        mutableStateOf(true)
+    }
+
+    androidx.compose.material.Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "코드랩에 오신 걸 환영합니다!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = { shouldShowOnBoarding = false }) {
+                Text(text = "Continue")
             }
         }
     }
@@ -31,13 +49,78 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    //리컴포지션을 방지하므로 상태가 재설정되지 않는다.
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    //배경 색상 설정
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+            OutlinedButton(onClick = {
+                expanded.value = !expanded.value
+            }) {
+                Text(if (expanded.value) "Show less" else "Show more")
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun SurfacedColumn(name: String) {
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(24.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(text = name)
+        }
+    }
+}
+
+@Composable
+fun TestColumn(titles: List<String> = listOf("World", "Compose", "Earth")) {
+    Column {
+        titles.forEach {
+            Greeting(name = it)
+        }
+    }
+}
+
+@Composable
+fun TestApp() {
+    var shouldShowOnBoarding by remember {
+        mutableStateOf(true)
+    }
+
+    if (shouldShowOnBoarding) {
+        OnBoardingScreen(onContinueCLicked = {
+            shouldShowOnBoarding = false
+        })
+    } else {
+        TestColumn()
+    }
+}
+
+//컴포즈 미리보기
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     BasicsCodelabTheme {
-        Greeting("Android")
+        Greeting("Lee Seung Yong")
     }
 }
